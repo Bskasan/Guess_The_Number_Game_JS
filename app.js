2887c717dfca3ab133c4fe2097de1abe28e799b4
Bskasan <b.kasan@yandex.com>
2023-01-21 17:10:53 +0200

    gues the number game saved

--- app.js
+++ app.js
@@ -1,54 +1,119 @@
-//*************** VARIABLES **************/
-
-const userInput = document.querySelector(".input");
-const button = document.querySelector(".input-con button");
+let randomNumber;
+const input = document.querySelector(".input");
+const buton = document.querySelector(".input-con button");
 const result = document.querySelector(".result");
 const imgCon = document.querySelector(".img-con");
+const startGame = document.querySelector(".btn-con button");
+const reStartGame = document.querySelector(".restart");
+const liveElement = document.querySelector("#live");
+const inputCon = document.querySelector(".input-con");
+const butonCon = document.querySelector(".btn-con");
+const firstNumSpan = document.querySelector(".first-number");
+const secondNumSpan = document.querySelector(".second-number");
+const guessText = document.querySelector(".guess-text");
 
-//? Get a number between 1 and 100.
-let randomNum = Math.ceil(Math.random() * 100);
-let liveNum = 3;
+let live;
+let firstNum;
+let secondNum;
 
-console.log(randomNum);
+startGame.addEventListener("click", () => {
+  live = 6;
+  liveElement.innerText = live;
+  butonCon.style.display = "none";
+  inputCon.style.display = "flex";
+  document.querySelector(".live-con").style.visibility = "visible";
+  result.innerText = "Write the number";
+  randomNumber = Math.ceil(Math.random() * 100);
+  buton.removeAttribute("disabled");
+  input.focus();
 
-//*************** EVENT LISTENER **************/
-button.addEventListener("click", () => {
-  console.log(userInput.value);
-  let inputValue = userInput.value;
-  liveNum--;
-  console.log("Live: ", liveNum);
+  firstNum = 0;
+  secondNum = 100;
+});
 
-  //if input is NaN
-  if (isNaN(inputValue)) {
-    console.log("Please enter a number");
+// restart game
+reStartGame.addEventListener("click", () => {
+  randomNumber = Math.ceil(Math.random() * 100);
+  imgCon.style.backgroundImage = "url('./assets/magic.png')";
+  butonCon.style.display = "none";
+  inputCon.style.display = "flex";
+  live = 6;
+  liveElement.innerText = live;
+  input.value = "";
+  result.innerText = "Write the number";
+  firstNum = 0;
+  secondNum = 100;
+  input.focus();
+  guessText.style.visibility = "hidden";
+});
+
+buton.addEventListener("click", () => {
+  live--;
+  liveElement.innerText = live;
+
+  // kazanma durumu
+  if (+input.value === randomNumber) {
+    result.innerText = "Congratulations";
+    imgCon.style.backgroundImage = "url('./images/congrat.gif')";
+    input.focus();
+    inputCon.style.display = "none";
+    butonCon.style.display = "flex";
+    reStartGame.style.display = "block";
+    startGame.style.display = "none";
+    guessText.style.visibility = "hidden";
   }
 
-  //Win
-  if (inputValue === randomNum) {
-    console.log("You win");
-    result.innerText = "Congratulations!";
-    imgCon.style.backgroundImage = "url('../images/congrat.gif')";
+  // tahmin edilen sayı küçükse
+
+  if (+input.value < randomNumber) {
+    guessText.style.visibility = "visible";
+    if (+input.value < 0) {
+      firstNum = 0;
+    } else if (+input.value > firstNum) {
+      firstNum = input.value;
+    }
+    firstNumSpan.innerHTML = firstNum;
+    secondNumSpan.innerHTML = secondNum;
+    result.innerText = "Try a higher number ";
+    imgCon.style.backgroundImage = "url('./images/higher.gif')";
+    input.value = "";
+    input.focus();
   }
 
-  //Lower Number
-  if (inputValue < randomNum) {
-    console.log("Higher");
-    result.innerText = "Please give higher number!";
-    imgCon.style.backgroundImage = "url('../images/higher.gif')";
+  // tahmin edilen sayı büyükse
+
+  if (+input.value > randomNumber) {
+    guessText.style.visibility = "visible";
+    if (+input.value > 100) {
+      secondNum = 100;
+    } else if (+input.value < secondNum) {
+      secondNum = input.value;
+    }
+    firstNumSpan.innerHTML = firstNum;
+    secondNumSpan.innerHTML = secondNum;
+    result.innerText = "Try a lower number ";
+    imgCon.style.backgroundImage = "url('./images/lower.gif')";
+    input.value = "";
+    input.focus();
   }
 
-  //Higher Number
-  if (inputValue > randomNum) {
-    console.log("Lower");
-    result.innerText = "Please give lower number!";
-    imgCon.style.backgroundImage = "url('../images/lower.gif')";
-    
+  // can bitince
+  if (live == 0) {
+    result.innerText = "Try Again!!";
+    imgCon.style.backgroundImage = "url('./images/tryagain.gif')"
+    input.value = "";
+    input.focus();
+    inputCon.style.display = "none";
+    butonCon.style.display = "flex";
+    reStartGame.style.display = "block";
+    startGame.style.display = "none";
+    guessText.style.visibility = "hidden";
+    buton.setAttribute("disabled", "true");
   }
+});
 
-  //if live is 0
-  if (liveNum == 0) {
-    console.log("You Lost");
-    result.innerText = "Try Again!";
-    imgCon.style.backgroundImage = "url('../images/tryagain.gif')";
+document.addEventListener("keydown", (el) => {
+  if (el.key == "Enter") {
+    buton.click();
   }
 });
